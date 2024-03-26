ed01ed5aa485da36d4a489f852dfc32999f87b88
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-03-26 11:49:07 -0300

    Correção

--- src/components/Pedro/main.js
+++ src/components/Pedro/main.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text, View } from 'react-native';
 
  export default function Pedro() {
   return (
